74756e4cfe8322fa6502029764e4f3d4e6bee3cf
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-10-04 01:00:35 -0400

    misc

--- sources/scripts/datapackage.py
+++ sources/scripts/datapackage.py
@@ -1,21 +1,24 @@
+import datetime
 import yaml
 import json
 import glob
-from os.path import join
+from os.path import join, splitext, basename
 
 import sys
-
-
 
 def main(src, dst):
     with open(join(src, 'root.yaml'), 'r') as f:
         print("reading root.yaml")
         data = yaml.load(f)
+        data['last_modified'] = datetime.datetime.today().isoformat()
     data['resources'] = []
     for filename in glob.glob('%s/resources/*.yaml' % src):
         print("reading %s" % filename)
         with open(filename, 'r') as f:
+            name = splitext(basename(filename))[0]
             res = yaml.load(f)
+            ## add name
+            res['name'] = name
             data['resources'].append(res)
     with open(dst, 'w') as f:
         print("writing to %s" % dst)
